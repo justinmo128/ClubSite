2354244dec70e65ac17159339c7a1f0b5c98be28
justinmo128 <112972855+justinmo128@users.noreply.github.com>
2022-10-08 22:17:13 -0600

    DO SOME MORE

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-document.getElementById("naviicon").addEventListener("click", myFunction)
-
 function myFunction() {
-    var x = document.getElementById("topnav");
-    if (x.className === "navilinkbox") {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
       x.className += " responsive";
     } else {
-      x.className = "navilinkbox";
+      x.className = "topnav";
     }
-    var  y = document.getElementById("toppernav");
-    if (y.className === "navi") {
-        y.className += " responsive";
-    } else {
-        y.className = "navi";
-    }
-  }
+}
