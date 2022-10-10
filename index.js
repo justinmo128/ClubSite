beef53f841edcefd237ad13fc870333d0010ea42
justinmo128 <112972855+justinmo128@users.noreply.github.com>
2022-10-09 21:15:34 -0600

    redone mobile navi #17

--- index.js
+++ index.js
@@ -1,41 +1,13 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-document.getElementById("icon").addEventListener("click", openNav);
+document.getElementById("navibutton").addEventListener("click", openNav);
 
 function openNav() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-}
-
-// Carousel
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  let x = document.getElementById("openmenu");
+  let y = document.getElementById("navibutton");
+  if (x.className === "openmenu") {
+    x.className += " opened"
+    y.style = "background-color: rgba(160, 46, 253, 0.4)"
+  } else {
+    x.className = "openmenu"
+    y.style = ""
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
