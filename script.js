315314cd3c1872e57a7a09cef63a5f33f7d77e02
justinmo128 <112972855+justinmo128@users.noreply.github.com>
2022-10-16 15:28:41 -0600

    hamburger cheeseburger big mac whopper

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 let mobileMenu = document.getElementById("mobilemenu")
+let menuthing = document.getElementsByClassName("menuthing");
 
 document.getElementById("menubutton").addEventListener("click", displayMenu)
-document.getElementById("closemenubutton").addEventListener("click", displayMenu)
+document.getElementById("closemenubutton").addEventListener("click", displayMenu) 
 
 function displayMenu() {
-    if (mobileMenu.style.display === "flex") {
-        mobileMenu.style.display = "none";
+    if (mobileMenu.style.left == "-320px") {
+        mobileMenu.style.left = "0";
+        mobileMenu.style.boxShadow = "0 0 0 1000px rgba(0, 0, 0, 0.5)";
+        menuthing[0].innerHTML = "menu_open";
+        menuthing[1].innerHTML = "menu_open";
     } else {
-        mobileMenu.style.display = "flex";
+        mobileMenu.style.left = "-320px";
+        mobileMenu.style.boxShadow = "0 0 0 1000px rgba(0, 0, 0, 0.0)";
+        menuthing[0].innerHTML = "menu";
+        menuthing[1].innerHTML = "menu";
     }
 }
