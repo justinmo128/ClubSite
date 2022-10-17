ec181a3258fc5d9998809fe719436f22d0985274
justinmo128 <112972855+justinmo128@users.noreply.github.com>
2022-10-17 16:24:50 -0600

    mobile menu closes when clicking empty area

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-let mobileMenu = document.getElementById("mobilemenu")
+let mobileMenu = document.getElementById("mobilemenu");
 let menuthing = document.getElementsByClassName("menuthing");
 
-document.getElementById("menubutton").addEventListener("click", displayMenu)
-document.getElementById("closemenubutton").addEventListener("click", displayMenu) 
+document.getElementById("menubutton").addEventListener("click", displayMenu);
+document.getElementById("closemenubutton").addEventListener("click", displayMenu);
+document.getElementById("closemenubox").addEventListener("mouseover", menuCloseOnHover);
+document.getElementById("closemenubox").addEventListener("click", closeMenu);
+mobileMenu.addEventListener("dblclick", closeMenu)
+
+function menuCloseOnHover() {
+    setTimeout(closeMenu, 250);
+}
 
 displayMenu();
 
@@ -12,10 +19,20 @@ function displayMenu() {
         mobileMenu.style.boxShadow = "0 0 0 1000px rgba(0, 0, 0, 0.5)";
         menuthing[0].innerHTML = "menu_open";
         menuthing[1].innerHTML = "menu_open";
+        document.getElementById("closemenubox").style.display = "block";
     } else {
         mobileMenu.style.left = "-320px";
         mobileMenu.style.boxShadow = "0 0 0 1000px rgba(0, 0, 0, 0.0)";
         menuthing[0].innerHTML = "menu";
         menuthing[1].innerHTML = "menu";
+        document.getElementById("closemenubox").style.display = "none";
     }
+}
+
+function closeMenu() {
+    mobileMenu.style.left = "-320px";
+    mobileMenu.style.boxShadow = "0 0 0 1000px rgba(0, 0, 0, 0.0)";
+    menuthing[0].innerHTML = "menu";
+    menuthing[1].innerHTML = "menu";
+    document.getElementById("closemenubox").style.display = "none";
 }
